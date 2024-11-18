686104b9b1fc0ca50abe0437a520d47cd73214f9
Stephen Ulibarri <druidmech@gmail.com>
2024-11-17 17:42:38 -0700

    Pretty Timers

--- Source/DedicatedServers/Private/UI/Timers/TimerWidget.cpp
+++ Source/DedicatedServers/Private/UI/Timers/TimerWidget.cpp
@@ -76,6 +76,7 @@ void UTimerWidget::TimerStarted(float InitialTime)
 {
 	bActive = true;
 	TextBlock_Time->SetRenderOpacity(1.f);
+	K2_OnTimerStarted(InitialTime, TimerType);
 }
 
 void UTimerWidget::TimerStopped()
